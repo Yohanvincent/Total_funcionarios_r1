c911d588748871ad5b6cf2a2e301ecf3609c7023
Yohanvincent <143143076+Yohanvincent@users.noreply.github.com>
2025-11-06 11:28:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.set_page_config(page_title="Dashboard Logístico", layout="wide")
-st.title("🏭 Dashboard Logístico")
+st.set_page_config(page_title="Capacidade x Mão de Obra", layout="wide")
+st.title("🏭 Capacidade x mão de obra")
 st.markdown("Use o **menu lateral** para navegar entre as páginas.")
