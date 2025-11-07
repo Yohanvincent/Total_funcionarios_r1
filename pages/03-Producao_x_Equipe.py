0a352d2d017ce59e23ec81f112a57dd2377219f9
Yohanvincent <143143076+Yohanvincent@users.noreply.github.com>
2025-11-07 17:29:35 -0300

    Update 03-Producao_x_Equipe.py

--- pages/03-Producao_x_Equipe.py
+++ pages/03-Producao_x_Equipe.py
@@ -3,6 +3,14 @@ import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
 import io
+from utils.auth import get_authenticator
+
+# === PROTEÇÃO ===
+authenticator = get_authenticator()
+name, authentication_status, username = authenticator.login("Login", "sidebar")
+
+if not authentication_status:
+    st.stop()  # PARA A EXECUÇÃO
 
 st.set_page_config(layout="wide")
 st.title("Produção vs Equipe Disponível")
